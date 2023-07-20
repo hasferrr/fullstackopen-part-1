cbb8404a13c7fd422c5d3ead36627c3d2c4665fe
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-07-20 17:21:07 +0700

    finish 1.9

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -2,9 +2,9 @@ import { useState } from 'react'
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(6)
-  const [neutral, setNeutral] = useState(2)
-  const [bad, setBad] = useState(1)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
@@ -18,6 +18,14 @@ const App = () => {
 }
 
 const Statistics = ({ good, neutral, bad, all }) => {
+  if (all === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
       <h1>statistics</h1>
