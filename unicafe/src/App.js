44a15ea30848ce2a98cb0f7eb68834bc0dfa15fe
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-07-20 16:51:30 +0700

    finish 1.7

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -2,9 +2,9 @@ import { useState } from 'react'
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(6)
+  const [neutral, setNeutral] = useState(2)
+  const [bad, setBad] = useState(1)
 
   return (
     <div>
@@ -16,6 +16,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good - bad) / (good + neutral + bad)}</p>
+      <p>positive {good / (good + neutral + bad) * 100 + "%"}</p>
     </div>
   )
 }
